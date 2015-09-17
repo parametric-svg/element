20fc22168afb8a311954f4fdff57210b954722cf
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-17 23:38:31 +0200

    Add two specs

--- test.js
+++ test.js
@@ -45,6 +45,59 @@ test('Registers the <parametric-svg> element', (is) => {
   register({document: {registerElement}});
 });
 
-'Updates the first SVG inside ASAP';
+// Register the thing on the actual `document`. That’s what `index.js` does.
+register({});
 
-'Doesn’t touch any other SVG';
+test('Works in a DOM structure created in one go', (is) => {
+  is.plan(1);
+
+  const parser = new DOMParser;
+  const parametricSvg = parser.parseFromString(
+`<!DOCTYPE html>
+<parametric-svg>
+  <svg>
+    <rect parametric:x="50" />
+  </svg>
+</parametric-svg>
+`,
+    'text/html'
+  );
+  const rect = parametricSvg.documentElement.querySelector('rect');
+
+  is.equal(
+    rect.getAttribute('x'),
+    '10',
+    'synchronously'
+  );
+});
+
+test('Works in a DOM structure built up programatically', (is) => {
+  is.plan(2);
+
+  const parametricSvg = document.createElement('parametric-svg');
+  const svg = document.createElement('svg');
+  const circle = document.createElement('circle');
+  svg.appendChild(circle);
+  parametricSvg.appendChild(svg);
+
+  circle.setAttribute('r', '5');
+  circle.setAttribute('parametric:r', '2 * (3 + 5)');
+
+  is.equal(
+    circle.getAttribute('r'),
+    '5',
+    'updates asynchronously'
+  );
+
+  requestAnimationFrame(() => {
+    is.equal(
+      circle.getAttribute('r'),
+      String(2 * (3 + 5)),
+      'within a single animation frame'
+    );
+  });
+});
+
+'Only affects the first child SVG';
+
+'Only affects a direct child SVG';
