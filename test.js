9e9da2fd7cfa4821d85aa4c6c0a7df22d839c17b
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-17 16:17:40 +0200

    Test document registration

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
-// const test = require('tape-catch');
+import register from './module/register';
 
-'Registers the element';
+const test = require('tape-catch');
+
+test('Registers the <parametric-svg> element', (is) => {
+  is.plan(2);
+
+  const registerElement = (
+    name, {extends: extendsArg = null}
+  ) => {
+    is.equal(
+      name,
+      'parametric-svg',
+      'under the name <parametric-svg>'
+    );
+
+    is.equal(
+      extendsArg,
+      null,
+      'as a standard HTML element – no bells and whistles'
+    );
+  };
+
+  register({document: {registerElement}});
+});
 
 'Updates the first SVG inside ASAP';
 
