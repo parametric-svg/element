e1563a16930c49a1a77f51fc5aeffd4129936e77
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-19 12:02:26 +0200

    Add specs for variables

--- test.js
+++ test.js
@@ -182,3 +182,62 @@ spec('Warns when no <svg> is inside', (test) => {
 
   register({document, HTMLElement, logger});
 });
+
+spec('Supports variables', (test) => {
+  test.plan(1);
+
+  document.body.innerHTML = `
+    <parametric-svg
+      a="2"
+      b="50"
+      >
+      <svg>
+        <rect parametric:x="b / a" />
+      </svg>
+    </parametric-svg>
+  `;
+
+  const rect = document.body.querySelector('rect');
+  test.equal(
+    rect.getAttribute('x'),
+    String(50 / 2),
+    'passes multiple variables in the right order'
+  );
+});
+
+spec('Only updates a parameter when all variables are defined', (test) => {
+  test.plan(3);
+
+  document.body.innerHTML = `
+    <parametric-svg
+      a="2"
+      >
+      <svg>
+        <circle parametric:r="a + b" r="5" />
+        <ellipse parametric:fill="c" />
+        <rect parametric:x="a" />
+      </svg>
+    </parametric-svg>
+  `;
+
+  const circle = document.body.querySelector('circle');
+  test.equal(
+    circle.getAttribute('r'),
+    '5',
+    'doesn’t update an attribute when its dependencies aren’t satisfied'
+  );
+
+  const ellipse = document.body.querySelector('ellipse');
+  test.equal(
+    ellipse.getAttribute('fill'),
+    null,
+    'doesn’t create an attribute when its dependencies aren’t satisfied'
+  );
+
+  const rect = document.body.querySelector('rect');
+  test.equal(
+    rect.getAttribute('x'),
+    '2',
+    'updates another attribute with satisfied dependencies`'
+  );
+});
